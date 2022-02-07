a347117973ad5400a679b5b1dcd1d234fbd9d5f7
Seversonic <43625790+Seversonic@users.noreply.github.com>
2022-02-07 09:59:09 -0700

    sampling available info

--- TwinCAT GUI/MainWindow.xaml.cs
+++ TwinCAT GUI/MainWindow.xaml.cs
@@ -13,10 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Linq;
 using TwinCAT;
 using TwinCAT.Ads;
 using TwinCAT.TypeSystem;
 using TwinCAT.Ads.TypeSystem;
+using TwinCAT.Ads.TcpRouter;
 
 namespace TwinCAT_GUI
 {
@@ -26,8 +28,9 @@ namespace TwinCAT_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private AdsClient adsClient; //
+        private AdsClient adsClient;
         private AdsClient adsSysSrv;
+        private AdsClient adsRouter;
         // private TcAdsSymbolInfoLoader symbolLoader;
 
         public MainWindow()
@@ -38,22 +41,52 @@ namespace TwinCAT_GUI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Debug.WriteLine("Test 1");
-
+         
+            //ComboboxPorts.ItemsSource = Enum.GetValues(typeof(AmsPort)).Cast<AmsPort>().ToDictionary(t => (int)t, t => t.ToString());
+            //ComboboxPorts.ItemsSource = Enum.GetValues(typeof(AmsPort));
+            //ComboboxPorts.ItemsSource = Enum.GetValues(typeof(AmsPort)).Cast<AmsPort>().ToLookup(t => t.ToString(), t => (int)t);
             try
             {
                 //connect to system service
                 adsSysSrv = new AdsClient();
-                adsSysSrv.Connect((int)AmsPort.PlcRuntime_851);
+                adsSysSrv.Connect((int)AmsPort.SystemService);
+                Debug.WriteLine(adsSysSrv.ReadState().ToString());
+                StateInfo test1 = adsSysSrv.ReadState();
+                Debug.WriteLine(test1.AdsState);
+                Debug.WriteLine(test1.DeviceState);
+                //connect to PLC
+                adsClient = new AdsClient();
+                adsClient.Connect((int)AmsPort.PlcRuntime_851);
+                Debug.WriteLine(adsClient.ReadState().ToString());
+                StateInfo test2 = adsClient.ReadState();
+                Debug.WriteLine(test2.AdsState);
+                Debug.WriteLine(test2.DeviceState);
+                //connect to other
+                adsRouter = new AdsClient();
+                adsRouter.Connect((int)AmsPort.Router);
+                Debug.WriteLine(adsRouter.ReadState().ToString());
+                StateInfo test3 = adsRouter.ReadState();
+                Debug.WriteLine(test3.AdsState);
+                Debug.WriteLine(test3.DeviceState);
                 //adsSysSrv.Synchronize = true;
                 Debug.WriteLine(adsSysSrv.Address);
                 Debug.WriteLine(adsSysSrv.ClientAddress);
                 Debug.WriteLine(adsSysSrv.IsConnected);
                 Debug.WriteLine(adsSysSrv.IsLocal);
                 Debug.WriteLine(adsSysSrv.Logger);
-                //Button_Click_1();
+                //Debug.WriteLine(adsSysSrv.);
+                Debug.WriteLine(adsRouter.Address);
+                Debug.WriteLine(adsRouter.ClientAddress);
+                Debug.WriteLine(adsRouter.IsConnected);
+                Debug.WriteLine(adsRouter.IsLocal);
+                Debug.WriteLine(adsRouter.Logger);
+                
 
 
                 ISymbolLoader loader = SymbolLoaderFactory.Create(adsSysSrv, SymbolLoaderSettings.Default);
+
+                //IAmsRouter routerRoutes = RouteCollection
+                //adsRouter.
 
                 foreach (Symbol symbol in loader.Symbols)
                 {
@@ -74,7 +107,7 @@ namespace TwinCAT_GUI
                         Debug.WriteLine(subSymbol.IndexGroup + " indexgrp");
                         Debug.WriteLine(subSymbol.HasValue + " hasvalue");
                         Debug.WriteLine(subSymbol.Attributes + " attributes");
-                        Debug.WriteLine(subSymbol.Category + " category");
+                        Debug.WriteLine(subSymbol.Category + " Parent");
                     }
                 }
 
@@ -90,6 +123,10 @@ namespace TwinCAT_GUI
 
         }
 
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 
 }
